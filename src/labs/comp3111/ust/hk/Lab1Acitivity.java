774c94b7d3872ebc76a4bcdfe5e4148e457df6ee
rep <rep@rep-PC>
2018-02-12 04:54:37 +0800

    Implemented the min/max functionalities

--- src/labs/comp3111/ust/hk/Lab1Acitivity.java
+++ src/labs/comp3111/ust/hk/Lab1Acitivity.java
@@ -12,26 +12,26 @@ public class Lab1Acitivity {
 		
 		System.out.println(String.format("Sum of the numbers is %d",sum));
 		
-//		int max=getMax(arr);int min=getMin(arr);
-//		System.out.println("Min = "+min+"; Max = "+max);
+		int max=getMax(arr);int min=getMin(arr);
+		System.out.println("Min = "+min+"; Max = "+max);
 		
 	}
 	
-//	public static int getMax(int[] inputArray) {
-//		int maxValue=inputArray[0];
-//		for(int i=1;i<inputArray.length;i++)
-//			if(inputArray[i]>maxValue)
-//				maxValue=inputArray[i];
-//		return maxValue;
-//	}
-//	
-//	 public static int getMin(int[] inputArray){ 
-//		    int minValue = inputArray[0]; 
-//		    for(int i=1;i<inputArray.length;i++) 
-//		      if(inputArray[i] < minValue) 
-//		        minValue = inputArray[i]; 
-//		       
-//		    
-//		    return minValue; 
-//		  } 
+	public static int getMax(int[] inputArray) {
+		int maxValue=inputArray[0];
+		for(int i=1;i<inputArray.length;i++)
+			if(inputArray[i]>maxValue)
+				maxValue=inputArray[i];
+		return maxValue;
+	}
+	
+	 public static int getMin(int[] inputArray){ 
+		    int minValue = inputArray[0]; 
+		    for(int i=1;i<inputArray.length;i++) 
+		      if(inputArray[i] < minValue) 
+		        minValue = inputArray[i]; 
+		       
+		    
+		    return minValue; 
+		  } 
 }
